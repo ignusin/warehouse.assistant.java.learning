d935a169226490743548556b084133ebe1b1d67a
Yakov Gnusin <y.gnusin@gmail.com>
2016-12-15 13:02:02 +0700

    Initial commit for JSP project

---
+++ jsp/src/main/java/iglabs/assistant/jsp/beans/NavigationBean.java
@@ -0,0 +1,40 @@
+package iglabs.assistant.jsp.beans;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.PageContext;
+
+public class NavigationBean {
+	
+	private PageContext pageContext;
+	
+	
+	public void setPageContext(PageContext pageContext) {
+		this.pageContext = pageContext;
+	}
+	
+	public PageContext getPageContext() {
+		return pageContext;
+	}
+	
+	private String getRelativePath() {
+		String requestUri = ((HttpServletRequest)pageContext.getRequest()).getRequestURI();
+		String contextPath = pageContext.getServletContext().getContextPath();
+		
+		String uri = requestUri.substring(contextPath.length());
+		return uri;
+	}
+	
+	private boolean relativePathEquals(String value) {
+		String uri = getRelativePath();
+		return uri.equals(value);
+	}
+	
+	private boolean relativePathContains(String value) {
+		String uri = getRelativePath();
+		return uri.contains(value);
+	}
+	
+	public boolean isItemsSection() {
+		return relativePathEquals("/") || relativePathContains("items");
+	}
+}
---
+++ jsp/src/main/webapp/pages/items.jsp
@@ -0,0 +1,6 @@
+<%@ page pageEncoding="UTF-8" %> 
+
+<h3>Номенклатура</h3>
+<div>
+
+</div>
